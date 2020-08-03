486d902334251c56dab2fbf0f607a22b7958aed3
Arunkumar <vparunvishal@gmail.com>
2020-08-04 01:24:28 +0530

    Add project files.

---
+++ BankAccount.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace UnitTestingBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            BankAccount ba = new BankAccount("Arunkumar",11.88); ;
+            ba.Credit(5.77);
+            ba.Debit(12.44);
+            Console.WriteLine("Current balance is ${0}", ba.Balance);
+
+
+        }
+    }
+
+    public class BankAccount
+    {
+        private readonly string m_customerName;
+        private double m_balance;
+
+        private BankAccount() { }
+
+        public BankAccount(string customerName, double balance)
+        {
+            m_customerName = customerName;
+            m_balance = balance;
+        }
+
+        public string CustomerName
+        {
+            get
+            {
+                return m_customerName;
+            }
+        }
+
+        public double Balance
+        {
+            get
+            {
+                return m_balance;
+            }
+        }
+
+        public void Debit(double amount)
+        {
+            if(amount > m_balance)
+            {
+                throw new ArgumentOutOfRangeException("amount");
+            }
+
+            if(amount <0)
+            {
+                throw new ArgumentOutOfRangeException("amount");
+            }
+            m_balance -= amount;
+        }
+
+        public void Credit(double amount)
+        {
+            if(amount<0)
+            {
+                throw new ArgumentOutOfRangeException("amount");
+            }
+            m_balance += amount;
+                
+        }
+    }
+
+
+}
